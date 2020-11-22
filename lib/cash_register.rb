d114ed206c372b91c055d9dbd71fa87fb9982b5f
logicfirst <slee@logicfirstllc.com>
2020-11-22 11:31:35 -0500

    commit tree 9f89dfd06d379ca43110e4f179de29e912882c46

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
+class CashRegister
 
+  attr_accessor :total, :discount, :title, :items, :transactions
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @transactions = []
+  end
+
+  def add_item(title, price, quantity = 1)
+    @items << (title * quantity)
+    @total = @total + (price * quantity)
+    @transactions << (price * quantity)
+  end
+
+  def apply_discount
+    if @discount == 0
+      "There is no discount to apply."
+    else @total = @total - @total * @discount/100
+    "After the discount, the total comes to $#{@total}."
+    end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    @total = @total - @transactions[-1] 
+  end
+
+end
